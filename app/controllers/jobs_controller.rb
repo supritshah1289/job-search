62a4cc7a8b0efe5223d8ac94ab02a3dc8fd61ebe
supritkumar shah <suprit7@gmail.com>
2016-08-01 15:47:00 -0400

    user can insert skills

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,9 +2,9 @@ class JobsController < ApplicationController
   def new
 
     keywords = params[:keywords]
-    puts keywords
-    api_key = "86fa3b366932e405519447d02cea3a8b"
-
+    puts params
+    api_key = ENV['api_key']
+    puts api_key
      url = "https://authenticjobs.com/api/?api_key=" +api_key + "+&method=aj.jobs.search&perpage=20"
     response = HTTParty.get(url)
     parsed_body = Crack::XML.parse(response.body)
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 # before_action :require_user, only: [:index, :show]
 
   def index
-
+    @user_skill = Skill.find_by(:user_id =>current_user.id)
   end
 
 end
